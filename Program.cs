8f56a25f5f076f48eb1425f5cb915e2ff140d700
oysterhead <savalos@gmail.com>
2023-12-13 22:37:01 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Desafio 1
+
+string nombre = "Sebastian";
+string apellido = "Avalos";
+int dni = 29208153;
+int edad = 42;
+double altura = 1.73;
+
+
+Console.WriteLine(nombre);
+Console.WriteLine(apellido);
+Console.WriteLine(dni);
+Console.WriteLine(edad);
+Console.WriteLine(altura);
+
+Console.ReadKey();
