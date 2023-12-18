075e7a6bcea8270bcf033f94ffb165d0c66a76bc
oysterhead <savalos@gmail.com>
2023-12-18 20:29:32 -0300

    Modificaciones realizadas.

--- Program.cs
+++ Program.cs
@@ -6,11 +6,13 @@ int dni = 29208153;
 int edad = 42;
 double altura = 1.73;
 
+Console.WriteLine("Bienvenidos a este Desafio Nro 1");
+Console.WriteLine("Aqui podrás encontrar mi informacion personal!!");
 
-Console.WriteLine(nombre);
-Console.WriteLine(apellido);
-Console.WriteLine(dni);
-Console.WriteLine(edad);
-Console.WriteLine(altura);
+Console.WriteLine("Me llamo: " + nombre);
+Console.WriteLine("Mi apellido paterno es: " + apellido);
+Console.WriteLine("Mi número de documento: " + dni);
+Console.WriteLine("Tengo " + edad + " años.");
+Console.WriteLine("Y Mido aproximadamente: " + altura);
 
 Console.ReadKey();
